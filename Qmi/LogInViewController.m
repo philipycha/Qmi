a6fc4272ba47e71347bef144da33a5aa6eeceffd
shauntc <shaun@shauntc.com>
2016-10-04 11:43:38 -0700

    basic storyboard for the login screen

---
+++ Qmi/LogInViewController.m
@@ -0,0 +1,37 @@
+//
+//  LogInViewController.m
+//  Qmi
+//
+//  Created by Shaun Campbell on 2016-10-04.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
