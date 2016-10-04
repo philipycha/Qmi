9d2cf1e40e146d6648ae4bb4951560dfe16b59c6
tevinmaker <tevin.maker@gmail.com>
2016-10-03 20:02:43 -0700

    location manager class added with starting of setup.

---
+++ Qmi/LocationManager.h
@@ -0,0 +1,21 @@
+//
+//  LocationManager.h
+//  Qmi
+//
+//  Created by Tevin Maker on 2016-10-03.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import CoreLocation;
+@import UIKit;
+
+@interface LocationManager : NSObject
+
+@property (nonatomic) CLLocationManager * locationManager;
+@property (nonatomic) CLLocation * currentLocation;
+
+
+-(void)setupLocationManager;
+
+@end
