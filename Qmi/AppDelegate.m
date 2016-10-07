df2908c00879e60891c587b1fdac99169a52a81d
Dan <dan.christal@gmail.com>
2016-10-06 17:43:35 -0700

    reset badge icon

--- Qmi/AppDelegate.m
+++ Qmi/AppDelegate.m
@@ -131,6 +131,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    //reset badge icon
+    application.applicationIconBadgeNumber = 0;
 }
 
 
